880546455594413675bf99e682245fa665a008ae
yukoshiono <mameyuko2525@gmail.com>
2015-09-12 07:16:03 +0000

    add edit implementation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
+
   def index
     #Messageを全て取得する
     @messages = Message.all
     @message = Message.new
   end
+
   def create
     @message = Message.new(message_params)
     if @message.save
@@ -15,9 +18,26 @@ class MessagesController < ApplicationController
       render 'index'
     end  
   end
-  
+
+  def edit
+  end  
+
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はトップページにリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      #保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end  
 end
