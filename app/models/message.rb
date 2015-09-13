97ad08bff7ed2f764b3d8ed71477368399c63a9e
yukoshiono <mameyuko2525@gmail.com>
2015-09-13 06:40:33 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length:{ maximum: 20 } , presence: true
     #内容は必須入力かつ２文字以上３０文字以下
     validates :body , length:{ minimum: 2 , maximum: 30} , presence: true
+    #年齢は必須入力かつ０以上130以下の数字のみ
+    validates :age , inclusion:{ in: 0..130} , numericality: { only_integer: true} , presence: true
 end
